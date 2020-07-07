4b14ef25ca95ac34ed1bdc6f85d490b23fdabd92
viniciusvivisantos <67937489+viniciusvivisantos@users.noreply.github.com>
2020-07-06 23:57:06 -0300

    Update compare-encrypted-passwords.ts

--- src/utils/compare-encrypted-passwords.ts
+++ src/utils/compare-encrypted-passwords.ts
@@ -1,4 +1,4 @@
-export const compareEncryptedPasswords = async (password1: string, password2) => {
+export const compareEncryptedPasswords = async (password1: string, password2: string) => {
     const md5 = require('md5');
     return md5(password1) == password2;
 }
